e49b913c765cfcf962f50957bd088d2de4347f3c
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-05-24 14:05:07 +0200

    Actualizo dialogo para que no salga el mensaje previo al nuevo metodo

--- src/Dialogo.java
+++ src/Dialogo.java
@@ -21,7 +21,7 @@ public class Dialogo extends JDialog {
     public static void crearDialogo(String mensaje) {
         Dialogo dialogo = new Dialogo();
         dialogo.pack();
-        dialogo.eVelocidad.setText("La velocidad es: "+mensaje);
+        dialogo.eVelocidad.setText(mensaje);
         dialogo.setVisible(true);
 
 
