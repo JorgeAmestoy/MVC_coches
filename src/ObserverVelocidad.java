e89e1ab6120d9598180c54701daf5f1ab595179d
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-05-25 21:00:05 +0200

    Creo clase ObserverVelocidad con el método update

---
+++ src/ObserverVelocidad.java
@@ -0,0 +1,27 @@
+import java.util.Observable;
+import java.util.Observer;
+
+//implemento Interfaz Observer para usar método update
+public class ObserverVelocidad implements Observer {
+
+    /**
+     * LLamamos a este cada vez que se produzca un cambio
+     * El observable(Model) cuando hace el notifyObservers
+     * activa los updates(actualizaciones) de los Observers
+     * @param o  es el observable, el Model
+     * @param arg es el coche actualizado, el arg pasado por
+     *           el observable
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+        //hay que castear el Objeto a tipo Coche
+        Coche auxCoche = (Coche) arg;
+        System.out.println("Se ha cambiado la velocidad "+auxCoche.velocidad.toString());
+        //llamamos a View para que muestra la velocidad
+        View.mostrarVelocidad(auxCoche.matricula, auxCoche.velocidad);
+
+        //en este metodo tambien accedemos al observable(el model),
+        //por lo que tenemos acceso al ArrayList de parking
+
+    }
+}
