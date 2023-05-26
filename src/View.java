572ee90e9efeea7f053188b5f9ab0fbe7504f113
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-05-26 09:53:55 +0200

    Creo nuevo metodo mostrarVelocidad

--- src/View.java
+++ src/View.java
@@ -15,7 +15,23 @@ public class View {
      */
    public static boolean mostrarVelocidad(String matricula, Integer v){
         System.out.println(matricula + ": " + v + "km/hr");
-        Dialogo.crearDialogo(String.valueOf(v));
+        Dialogo.crearDialogo("La velocidad es: "+String.valueOf(v));
         return true;
     }
+
+    /**
+     * Metodo que muestra la velocidad
+     * y que avisa si se ha superadp
+     * @param matricula del coche
+     * @param v velocidad
+     * @return true si es verdadero
+     */
+    public static boolean mostrarVelocidad2(String matricula, Integer v){
+        System.out.println(matricula + ": " + v + "km/hr");
+        Dialogo.crearDialogo("Has superado!!");
+        return true;
+    }
+
+
+
 }
